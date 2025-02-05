fa4041b465c85e2cb881da342e5a5b1a4659f2f4
emmanuel <badejoemmanuel805@gmail.com>
2025-02-04 18:06:01 -0800

    mend

--- cloud_store/settings.py
+++ cloud_store/settings.py
@@ -79,12 +79,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
